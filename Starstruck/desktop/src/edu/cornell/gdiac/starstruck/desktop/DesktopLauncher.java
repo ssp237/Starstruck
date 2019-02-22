570a05b2cfeea811b65e9af323e36e9365363678
ssp237 <ssp237@cornell.edu>
2019-02-22 16:08:57 -0500

    game stuff

---
+++ Starstruck/desktop/src/edu/cornell/gdiac/starstruck/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.cornell.gdiac.starstruck.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.cornell.gdiac.starstruck.GDXRoot;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GDXRoot(), config);
+	}
+}
