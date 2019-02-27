2b6d99b879fbeafbb37b7a6b92602a16fe1ead37
ssp237 <ssp237@cornell.edu>
2019-02-26 22:03:25 -0500

    planet pizza?

---
+++ PlanetDemo/desktop/src/edu/cornell/gdiac/planetdemo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.cornell.gdiac.planetdemo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.cornell.gdiac.planetdemo.GDXRoot;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GDXRoot(), config);
+	}
+}
