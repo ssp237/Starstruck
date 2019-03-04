fca912f1f67737cfb8efc0eea76fe87eeeed8b69
Joy Zhang <jz442@cornell.edu>
2019-03-03 23:43:11 -0500

    i'm a dumbass?

--- PlanetDemo/core/src/edu/cornell/gdiac/planetdemo/DudeModel.java
+++ PlanetDemo/core/src/edu/cornell/gdiac/planetdemo/DudeModel.java
@@ -367,7 +367,7 @@ public class DudeModel extends CapsuleObstacle {
         // Gravity from planets
         //System.out.println(gravity);
         if (!getOnPlanet())
-            body.applyForce(gravity.setLength(3), getPosition(), true);
+            body.applyForce(gravity, getPosition(), true);
 
     }
 
