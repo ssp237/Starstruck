df47c99b3da1ac5781e427e19cbe3c7acef266c2
Joy Zhang <jz442@cornell.edu>
2019-03-31 21:50:28 -0400

    PlayerModel doesn't actually do anything

---
+++ Starstruck/core/src/edu/cornell/gdiac/starstruck/PlayerModel.java
@@ -0,0 +1,27 @@
+package edu.cornell.gdiac.starstruck;
+
+import com.badlogic.gdx.math.*;
+import com.badlogic.gdx.utils.*;
+import com.badlogic.gdx.audio.*;
+import com.badlogic.gdx.assets.*;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.physics.box2d.*;
+
+import java.util.*;
+
+//import edu.cornell.gdiac.physics.*;
+import com.sun.org.apache.bcel.internal.generic.LAND;
+import edu.cornell.gdiac.util.*;
+import edu.cornell.gdiac.starstruck.Obstacles.*;
+import edu.cornell.gdiac.starstruck.Gravity.*;
+import edu.cornell.gdiac.util.FilmStrip;
+
+public class PlayerModel {
+
+    private AstronautModel avatar1;
+    private AstronautModel avatar2;
+    private Rope rope;
+
+
+}
